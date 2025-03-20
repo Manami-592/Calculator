4d4d00de2a4503e961b89382970927c47e09140c
Manami-592 <kaimanyami@icloud.com>
2025-03-20 16:18:33 +0900

    2つの数字と1つの演算子入力で計算

--- Calculator/src/main.java
+++ Calculator/src/main.java
@@ -1,7 +1,34 @@
+import java.util.Scanner;
 
 public class main {
 
 	public static void main(String[] args) {
+		Scanner str = new Scanner(System.in);
+		System.out.print("数字を入力してください。");
+		int a = str.nextInt();
+		
+		System.out.print("演算子を入力してください。");
+		String ope = str.next();
+		
+		System.out.print("数字を入力してください。");
+		int b = str.nextInt();
+		
+		if(ope.equals("+")) {
+			System.out.println(Calculation.add(a, b));
+			
+		} else if(ope.equals("-")) {
+			System.out.println(Calculation.sub(a, b));
+			
+		} else if(ope.equals("×")) {
+			System.out.println(Calculation.mul(a, b));
+			
+		} else if(ope.equals("÷")) {
+			try {
+				System.out.println(Calculation.div(a, b));
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
 	}
 
 }
